c8fb7e85de4483782ce6404b896e5e1c864cbae5
Jérémie Astori <jeremie@astori.fr>
2016-03-02 21:37:48 +0000

    Add a bash alias file

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ set -e # Stop execution at first error
 
 dotfiles="$PWD"
 
+ln -sf ${dotfiles}/bash/bash_aliases ~/.bash_aliases
+
 ln -sf ${dotfiles}/git/gitconfig ~/.gitconfig
 ln -sf ${dotfiles}/git/gitignore_global ~/.gitignore_global
 
