e2eefb5f832124d863dc95d68a014e059339ae90
Jérémie Astori <jeremie@astori.fr>
2016-03-30 13:40:30 -0400

    Add MySQL configuration to avoid a recurring issue on Mac OSX

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,12 @@ dotfiles="$PWD"
 
 ln -sf ${dotfiles}/bash/bash_aliases ~/.bash_aliases
 
+# Git
 ln -sf ${dotfiles}/git/gitconfig ~/.gitconfig
 ln -sf ${dotfiles}/git/gitignore_global ~/.gitignore_global
 
+# Ruby
 ln -sf ${dotfiles}/ruby/pryrc ~/.pryrc
+
+# MySQL
+ln -sf ${dotfiles}/mysql/my.cnf ~/.my.cnf
