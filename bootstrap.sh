365cd0d6dbb9ed5637d58830234bf28932185f1b
Jérémie Astori <jeremie@astori.fr>
2018-10-17 15:14:14 -0400

    Set up git signing

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ set -e # Stop execution at first error
 
 dotfiles="$PWD"
 
+brew install bash-completion diff-so-fancy git gnupg pinentry-mac
+
 ln -sf ${dotfiles}/bash/bash_aliases ~/.bash_aliases
 ln -sf ${dotfiles}/bash/bashrc ~/.bashrc
 ln -sf ${dotfiles}/bash/bash_profile ~/.bash_profile
@@ -11,6 +13,11 @@ ln -sf ${dotfiles}/bash/bash_profile ~/.bash_profile
 # Git
 ln -sf ${dotfiles}/git/gitconfig ~/.gitconfig
 ln -sf ${dotfiles}/git/gitignore_global ~/.gitignore_global
+
+# Git signing with GPG
+brew link --overwrite gnupg
+ln -sf ${dotfiles}/git/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+killall gpg-agent
 
 # Ruby
 ln -sf ${dotfiles}/ruby/pryrc ~/.pryrc
